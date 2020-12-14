99b6bbde9738bc6668052b979d087b9dedea9590
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-12-14 21:43:20 +0100

    add docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [
